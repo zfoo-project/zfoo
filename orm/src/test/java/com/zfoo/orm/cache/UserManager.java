3cab1178c51c6ff111794887c9b2a32f047174f9
jaysunxiao <jaysunxiao@gmail.com>
2021-06-24 12:10:57 +0800

    perf[orm]: 删除无用代码和调整目录结构

--- orm/src/test/java/com/zfoo/orm/cache/UserManager.java
+++ orm/src/test/java/com/zfoo/orm/cache/UserManager.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.zfoo.orm.test;
+package com.zfoo.orm.cache;
 
 import com.zfoo.orm.entity.UserEntity;
 import com.zfoo.orm.model.anno.EntityCachesInjection;
