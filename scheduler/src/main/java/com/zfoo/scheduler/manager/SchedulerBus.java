749df8ef24e770427f238b20a2194eb6df35f68b
islandempty <2667114200@qq.com>
2021-10-09 22:30:32 +0800

    fix[scheduler]:修改为基于时间轮算法的定时任务调度

--- scheduler/src/main/java/com/zfoo/scheduler/manager/SchedulerBus.java
+++ scheduler/src/main/java/com/zfoo/scheduler/manager/SchedulerBus.java
@@ -16,6 +16,8 @@ package com.zfoo.scheduler.manager;
 import com.zfoo.protocol.collection.CollectionUtils;
 import com.zfoo.scheduler.SchedulerContext;
 import com.zfoo.scheduler.model.vo.SchedulerDefinition;
+import com.zfoo.scheduler.timeWheelUtils.Timer;
+import com.zfoo.scheduler.timeWheelUtils.TimerTask;
 import com.zfoo.scheduler.util.TimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,21 +36,7 @@ public abstract class SchedulerBus {
 
     private static final Logger logger = LoggerFactory.getLogger(SchedulerBus.class);
 
-    private static final List<SchedulerDefinition> schedulerDefList = new CopyOnWriteArrayList<>();
-
-    /**
-     * 上一次trigger触发时间
-     */
-    private static long lastTriggerTimestamp = 0L;
-
-
-    /**
-     * 在scheduler中，最小的triggerTimestamp
-     */
-    private static long minTriggerTimestamp = 0L;
-
-
-    public static final long TRIGGER_MILLIS_INTERVAL = TimeUtils.MILLIS_PER_SECOND;
+    private static Timer timer = new Timer();
 
     /**
      * scheduler默认只有一个单线程的线程池
@@ -56,75 +44,12 @@ public abstract class SchedulerBus {
     private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new SchedulerThreadFactory(1));
 
 
-    static {
-        executor.scheduleAtFixedRate(() -> {
-            try {
-                triggerPerSecond();
-            } catch (Exception e) {
-                logger.error("scheduler triggers an error.", e);
-            }
-        }, 0, TRIGGER_MILLIS_INTERVAL, TimeUnit.MILLISECONDS);
-    }
-
-
-    public static void refreshMinTriggerTimestamp() {
-        var minTimestamp = Long.MAX_VALUE;
-        for (var scheduler : schedulerDefList) {
-            if (scheduler.getTriggerTimestamp() < minTimestamp) {
-                minTimestamp = scheduler.getTriggerTimestamp();
-            }
-        }
-        minTriggerTimestamp = minTimestamp;
-    }
-
-    /**
-     * 每一秒执行一次，如果这个任务执行时间过长超过，比如10秒，执行完成后，不会再执行10次
-     */
-    private static void triggerPerSecond() {
-        var timestamp = TimeUtils.currentTimeMillis();
-
-        if (CollectionUtils.isEmpty(schedulerDefList)) {
-            return;
-        }
-
-
-        // 有人向前调整过机器时间，重新计算scheduler里的triggerTimestamp
-        // var diff = timestamp - lastTriggerTimestamp;
-        if (timestamp < lastTriggerTimestamp) {
-            for (SchedulerDefinition schedulerDef : schedulerDefList) {
-                var nextTriggerTimestamp = TimeUtils.getNextTimestampByCronExpression(schedulerDef.getCronExpression(), timestamp);
-                schedulerDef.setTriggerTimestamp(nextTriggerTimestamp);
-            }
-            refreshMinTriggerTimestamp();
-        }
-
-        // diff > 0, 没有人调整时间或者有人向后调整过机器时间，可以忽略，因为向后调整时间时间戳一定会大于triggerTimestamp，所以一定会触发
-        lastTriggerTimestamp = timestamp;
-
-        // 如果minSchedulerTriggerTimestamp大于timestamp，说明没有可执行的scheduler
-        if (timestamp < minTriggerTimestamp) {
-            return;
-        }
-
-        var minTimestamp = Long.MAX_VALUE;
-        for (var scheduler : schedulerDefList) {
-            if (timestamp >= scheduler.getTriggerTimestamp()) {
-                // 到达触发时间，则执行runnable方法
-                scheduler.getScheduler().invoke();
-                // 重新设置下一次的触发时间戳
-                var nextTriggerTimestamp = TimeUtils.getNextTimestampByCronExpression(scheduler.getCronExpression(), timestamp);
-                scheduler.setTriggerTimestamp(nextTriggerTimestamp);
-            }
-            if (scheduler.getTriggerTimestamp() < minTimestamp) {
-                minTimestamp = scheduler.getTriggerTimestamp();
-            }
-        }
-        minTriggerTimestamp = minTimestamp;
-    }
-
     public static void registerScheduler(SchedulerDefinition scheduler) {
-        schedulerDefList.add(scheduler);
-        refreshMinTriggerTimestamp();
+        var timerTask = new TimerTask(scheduler.getTriggerTimestamp(), () -> {
+            scheduler.getScheduler().invoke();
+            refreshTask(scheduler);
+        });
+        timer.addTask(timerTask);
     }
 
 
@@ -181,6 +106,22 @@ public abstract class SchedulerBus {
             return;
         }
 
-        schedulerDefList.add(SchedulerDefinition.valueOf(cron, runnable));
+        SchedulerDefinition scheduler = SchedulerDefinition.valueOf(cron, runnable);
+        var timerTask = new TimerTask(scheduler.getTriggerTimestamp(), () -> {
+            scheduler.getScheduler().invoke();
+            refreshTask(scheduler);
+        });
+        timer.addTask(timerTask);
+    }
+
+    public static void refreshTask(SchedulerDefinition schedulerDefinition) {
+        var timestamp = TimeUtils.currentTimeMillis();
+        var nextTriggerTimestamp = TimeUtils.getNextTimestampByCronExpression(schedulerDefinition.getCronExpression(), timestamp);
+        schedulerDefinition.setTriggerTimestamp(nextTriggerTimestamp);
+        var timerTask = new TimerTask(schedulerDefinition.getTriggerTimestamp(), () -> {
+            schedulerDefinition.getScheduler().invoke();
+            refreshTask(schedulerDefinition);
+        });
+        timer.addTask(timerTask);
     }
 }
