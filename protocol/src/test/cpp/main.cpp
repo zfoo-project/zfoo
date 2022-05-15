6a8aa811a0585714acafa9aed7747d3a6d9570a5
jaysunxiao <jaysunxiao.gmail.com>
2022-05-15 21:59:02 +0800

    feat[protocol]: 支持C++序列化协议

---
+++ protocol/src/test/cpp/main.cpp
@@ -0,0 +1,18 @@
+#include "test/list_test.h"
+#include "test/byte_buffer_test.h"
+#include "test/serialization_test.h"
+#include "test/speed_test.h"
+
+using namespace zfoo;
+using namespace std;
+
+
+int main() {
+    list_test::list_all_test();
+    byte_buffer_test::byte_buffer_all_test();
+    serialization_test::protocol_all_test();
+
+    speed_test::parseObject();
+    speed_test::singleThreadBenchmarks();
+    return 0;
+}
---
+++ protocol/src/test/cpp/zfoocpp/ProtocolManager.h
@@ -0,0 +1,42 @@
+#ifndef ZFOO_PROTOCOLMANAGER_H
+#define ZFOO_PROTOCOLMANAGER_H
+
+#include "ByteBuffer.h"
+#include "zfoocpp/ComplexObject.h"
+#include "zfoocpp/NormalObject.h"
+#include "zfoocpp/ObjectA.h"
+#include "zfoocpp/ObjectB.h"
+#include "zfoocpp/SimpleObject.h"
+
+namespace zfoo {
+
+    const int16_t MAX_PROTOCOL_NUM = 32767;
+    const IProtocolRegistration *protocols[MAX_PROTOCOL_NUM];
+
+    void initProtocol() {
+        protocols[100] = new ComplexObjectRegistration();
+        protocols[101] = new NormalObjectRegistration();
+        protocols[102] = new ObjectARegistration();
+        protocols[103] = new ObjectBRegistration();
+        protocols[104] = new SimpleObjectRegistration();
+    }
+
+    inline IProtocolRegistration *getProtocol(int16_t protocolId) {
+        return const_cast<IProtocolRegistration *>(protocols[protocolId]);
+    }
+
+    void write(ByteBuffer &buffer, IPacket *packet) {
+        auto protocolId = packet->protocolId();
+        // 写入协议号
+        buffer.writeShort(protocolId);
+        // 写入包体
+        getProtocol(protocolId)->write(buffer, packet);
+    }
+
+    IPacket *read(ByteBuffer &buffer) {
+        auto protocolId = buffer.readShort();
+        return getProtocol(protocolId)->read(buffer);
+    }
+
+}
+#endif
