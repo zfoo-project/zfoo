5eb2530eaeee41aeff2e9756ef75819090b8a81b
jaysunxiao <jaysunxiao.gmail.com>
2022-05-16 23:15:24 +0800

    perf[protocol]: 使用模板生成协议

---
+++ protocol/src/main/java/com/zfoo/protocol/serializer/csharp/GenerateCsUtils.java
@@ -0,0 +1,309 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.protocol.serializer.csharp;
+
+import com.zfoo.protocol.generate.GenerateOperation;
+import com.zfoo.protocol.generate.GenerateProtocolDocument;
+import com.zfoo.protocol.generate.GenerateProtocolFile;
+import com.zfoo.protocol.generate.GenerateProtocolPath;
+import com.zfoo.protocol.model.Pair;
+import com.zfoo.protocol.registration.ProtocolRegistration;
+import com.zfoo.protocol.serializer.reflect.*;
+import com.zfoo.protocol.util.ClassUtils;
+import com.zfoo.protocol.util.FileUtils;
+import com.zfoo.protocol.util.IOUtils;
+import com.zfoo.protocol.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.zfoo.protocol.util.FileUtils.LS;
+import static com.zfoo.protocol.util.StringUtils.TAB;
+
+/**
+ * @author jaysunxiao
+ * @version 3.0
+ */
+public abstract class GenerateCsUtils {
+
+    private static String protocolOutputRootPath = "CsProtocol/";
+
+    private static Map<ISerializer, ICsSerializer> csSerializerMap;
+
+    public static ICsSerializer csSerializer(ISerializer serializer) {
+        return csSerializerMap.get(serializer);
+    }
+
+    public static void init(GenerateOperation generateOperation) {
+        protocolOutputRootPath = FileUtils.joinPath(generateOperation.getProtocolPath(), protocolOutputRootPath);
+
+        FileUtils.deleteFile(new File(protocolOutputRootPath));
+        FileUtils.createDirectory(protocolOutputRootPath);
+
+        csSerializerMap = new HashMap<>();
+        csSerializerMap.put(BooleanSerializer.INSTANCE, new CsBooleanSerializer());
+        csSerializerMap.put(ByteSerializer.INSTANCE, new CsByteSerializer());
+        csSerializerMap.put(ShortSerializer.INSTANCE, new CsShortSerializer());
+        csSerializerMap.put(IntSerializer.INSTANCE, new CsIntSerializer());
+        csSerializerMap.put(LongSerializer.INSTANCE, new CsLongSerializer());
+        csSerializerMap.put(FloatSerializer.INSTANCE, new CsFloatSerializer());
+        csSerializerMap.put(DoubleSerializer.INSTANCE, new CsDoubleSerializer());
+        csSerializerMap.put(CharSerializer.INSTANCE, new CsCharSerializer());
+        csSerializerMap.put(StringSerializer.INSTANCE, new CsStringSerializer());
+        csSerializerMap.put(ArraySerializer.INSTANCE, new CsArraySerializer());
+        csSerializerMap.put(ListSerializer.INSTANCE, new CsListSerializer());
+        csSerializerMap.put(SetSerializer.INSTANCE, new CsSetSerializer());
+        csSerializerMap.put(MapSerializer.INSTANCE, new CsMapSerializer());
+        csSerializerMap.put(ObjectProtocolSerializer.INSTANCE, new CsObjectProtocolSerializer());
+    }
+
+    public static void clear() {
+        csSerializerMap = null;
+        protocolOutputRootPath = null;
+    }
+
+    /**
+     * 生成协议依赖的工具类
+     */
+    public static void createProtocolManager() throws IOException {
+        var list = List.of("csharp/ProtocolManager.cs"
+                , "csharp/IProtocolRegistration.cs"
+                , "csharp/IPacket.cs"
+                , "csharp/Buffer/ByteBuffer.cs"
+                , "csharp/Buffer/LittleEndianByteBuffer.cs"
+                , "csharp/Buffer/BigEndianByteBuffer.cs");
+
+        for (var fileName : list) {
+            var fileInputStream = ClassUtils.getFileFromClassPath(fileName);
+            var createFile = new File(StringUtils.format("{}/{}", protocolOutputRootPath, StringUtils.substringAfterFirst(fileName, "csharp/")));
+            FileUtils.writeInputStreamToFile(createFile, fileInputStream);
+        }
+    }
+
+    /**
+     * 生成协议类
+     */
+    public static void createCsProtocolFile(ProtocolRegistration registration) throws IOException {
+        GenerateProtocolFile.index.set(0);
+
+        var protocolId = registration.protocolId();
+        var registrationConstructor = registration.getConstructor();
+        var protocolClazzName = registrationConstructor.getDeclaringClass().getSimpleName();
+
+        var protocolTemplate = StringUtils.bytesToString(IOUtils.toByteArray(ClassUtils.getFileFromClassPath("csharp/ProtocolTemplate.cs")));
+
+        var docTitle = docTitle(registration);
+        var fieldDefinition = fieldDefinition(registration);
+        var valueOfMethod = valueOfMethod(registration);
+        var writeObject = writeObject(registration);
+        var readObject = readObject(registration);
+        protocolTemplate = StringUtils.format(protocolTemplate, docTitle, protocolClazzName,  fieldDefinition.trim()
+                , protocolClazzName, valueOfMethod.getKey().trim(), protocolClazzName, valueOfMethod.getValue().trim()
+                , protocolId, protocolClazzName, protocolId, protocolClazzName, protocolClazzName,writeObject.trim()
+                , protocolClazzName, protocolClazzName, readObject.trim());
+
+        var protocolOutputPath = StringUtils.format("{}/{}/{}.cs"
+                , protocolOutputRootPath
+                , GenerateProtocolPath.getCapitalizeProtocolPath(protocolId)
+                , protocolClazzName);
+        FileUtils.writeStringToFile(new File(protocolOutputPath), protocolTemplate);
+    }
+
+    private static String docTitle(ProtocolRegistration registration) {
+        var protocolId = registration.getId();
+        var protocolDocument = GenerateProtocolDocument.getProtocolDocument(protocolId);
+        var docTitle = protocolDocument.getKey();
+
+        var csBuilder = new StringBuilder();
+        if (StringUtils.isNotBlank(docTitle)) {
+            Arrays.stream(docTitle.split(LS)).forEach(it -> csBuilder.append(TAB).append(it).append(LS));
+        }
+        return csBuilder.toString().trim();
+    }
+
+    private static String fieldDefinition(ProtocolRegistration registration) {
+        var protocolId = registration.getId();
+        var fields = registration.getFields();
+        var protocolDocument = GenerateProtocolDocument.getProtocolDocument(protocolId);
+        var docFieldMap = protocolDocument.getValue();
+
+        var csBuilder = new StringBuilder();
+        // 协议的属性生成
+        for (var field : fields) {
+            var propertyType = toCsClassName(field.getGenericType().getTypeName());
+            var propertyName = field.getName();
+            var propertyFullName = StringUtils.format("public {} {};", propertyType, propertyName);
+            // 生成注释
+            var doc = docFieldMap.get(propertyName);
+            if (StringUtils.isNotBlank(doc)) {
+                Arrays.stream(doc.split(LS)).forEach(it -> csBuilder.append(TAB + TAB).append(it).append(LS));
+            }
+            csBuilder.append(TAB + TAB).append(propertyFullName).append(LS);
+        }
+        return csBuilder.toString();
+    }
+
+    private static Pair<String, String> valueOfMethod(ProtocolRegistration registration) {
+        var fields = registration.getFields();
+        var filedList = new ArrayList<Pair<String, String>>();
+        for (var field : fields) {
+            var propertyType = toCsClassName(field.getGenericType().getTypeName());
+            var propertyName = field.getName();
+            filedList.add(new Pair<>(propertyType, propertyName));
+        }
+
+        // ValueOf()方法
+        var valueOfParams = filedList.stream().map(it -> StringUtils.format("{} {}", it.getKey(), it.getValue())).collect(Collectors.toList());
+        var valueOfParamsStr = StringUtils.joinWith(StringUtils.COMMA + " ", valueOfParams.toArray());
+
+        var csBuilder = new StringBuilder();
+        filedList.forEach(it -> csBuilder.append(TAB + TAB + TAB).append(StringUtils.format("packet.{} = {};", it.getValue(), it.getValue())).append(LS));
+        return new Pair<>(valueOfParamsStr, csBuilder.toString());
+    }
+
+    private static String writeObject(ProtocolRegistration registration) {
+        var fields = registration.getFields();
+        var fieldRegistrations = registration.getFieldRegistrations();
+        var csBuilder = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            var fieldRegistration = fieldRegistrations[i];
+            csSerializer(fieldRegistration.serializer()).writeObject(csBuilder, "message." + field.getName(), 3, field, fieldRegistration);
+        }
+        return csBuilder.toString();
+    }
+
+
+    private static String readObject(ProtocolRegistration registration) {
+        var fields = registration.getFields();
+        var fieldRegistrations = registration.getFieldRegistrations();
+        var csBuilder = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            var fieldRegistration = fieldRegistrations[i];
+            String readObject = csSerializer(fieldRegistration.serializer()).readObject(csBuilder, 3, field, fieldRegistration);
+            csBuilder.append(TAB + TAB + TAB)
+                    .append(StringUtils.format("packet.{} = {};", field.getName(), readObject))
+                    .append(LS);
+        }
+        return csBuilder.toString();
+    }
+
+    public static String toCsClassName(String typeName) {
+        typeName = typeName.replaceAll("java.util.|java.lang.", StringUtils.EMPTY);
+        typeName = typeName.replaceAll("com\\.[a-zA-Z0-9_.]*\\.", StringUtils.EMPTY);
+
+        // CSharp不适用基础类型的泛型，会影响性能
+        switch (typeName) {
+            case "boolean":
+            case "Boolean":
+                typeName = "bool";
+                return typeName;
+            case "Byte":
+                typeName = "byte";
+                return typeName;
+            case "Short":
+                typeName = "short";
+                return typeName;
+            case "Integer":
+                typeName = "int";
+                return typeName;
+            case "Long":
+                typeName = "long";
+                return typeName;
+            case "Float":
+                typeName = "float";
+                return typeName;
+            case "Double":
+                typeName = "double";
+                return typeName;
+            case "Character":
+                typeName = "char";
+                return typeName;
+            case "String":
+                typeName = "string";
+                return typeName;
+            default:
+        }
+
+        // 将boolean转为bool
+        typeName = typeName.replace(" boolean ", " bool ");
+        typeName = typeName.replace(" Boolean ", " bool ");
+        typeName = typeName.replaceAll("[B|b]oolean\\[", "bool[");
+        typeName = typeName.replace("<Boolean", "<bool");
+        typeName = typeName.replace("Boolean>", "bool>");
+
+        // 将Byte转为byte
+        typeName = typeName.replace(" Byte ", " byte ");
+        typeName = typeName.replace("Byte[", "byte[");
+        typeName = typeName.replace("Byte>", "byte>");
+        typeName = typeName.replace("<Byte", "<byte");
+
+        // 将Short转为short
+        typeName = typeName.replace(" Short ", " short ");
+        typeName = typeName.replace("Short[", "short[");
+        typeName = typeName.replace("Short>", "short>");
+        typeName = typeName.replace("<Short", "<short");
+
+        // 将Integer转为int
+        typeName = typeName.replace(" Integer ", " int ");
+        typeName = typeName.replace("Integer[", "int[");
+        typeName = typeName.replace("Integer>", "int>");
+        typeName = typeName.replace("<Integer", "<int");
+
+
+        // 将Long转为long
+        typeName = typeName.replace(" Long ", " long ");
+        typeName = typeName.replace("Long[", "long[");
+        typeName = typeName.replace("Long>", "long>");
+        typeName = typeName.replace("<Long", "<long");
+
+        // 将Float转为float
+        typeName = typeName.replace(" Float ", " float ");
+        typeName = typeName.replace("Float[", "float[");
+        typeName = typeName.replace("Float>", "float>");
+        typeName = typeName.replace("<Float", "<float");
+
+        // 将Double转为double
+        typeName = typeName.replace(" Double ", " double ");
+        typeName = typeName.replace("Double[", "double[");
+        typeName = typeName.replace("Double>", "double>");
+        typeName = typeName.replace("<Double", "<double");
+
+        // 将Character转为Char
+        typeName = typeName.replace(" Character ", " char ");
+        typeName = typeName.replace("Character[", "char[");
+        typeName = typeName.replace("Character>", "char>");
+        typeName = typeName.replace("<Character", "<char");
+
+        // 将String转为string
+        typeName = typeName.replace("String ", "string ");
+        typeName = typeName.replace("String[", "string[");
+        typeName = typeName.replace("String>", "string>");
+        typeName = typeName.replace("<String", "<string");
+
+        // 将Map转为Dictionary
+        typeName = typeName.replace("Map<", "Dictionary<");
+
+        // 将Set转为HashSet
+        typeName = typeName.replace("Set<", "HashSet<");
+
+        // 将private转为public
+        typeName = typeName.replace(" private ", " public ");
+
+        return typeName;
+    }
+}
---
+++ protocol/src/main/java/com/zfoo/protocol/serializer/javascript/GenerateJsUtils.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright (C) 2020 The zfoo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.zfoo.protocol.serializer.javascript;
+
+import com.zfoo.protocol.generate.GenerateOperation;
+import com.zfoo.protocol.generate.GenerateProtocolDocument;
+import com.zfoo.protocol.generate.GenerateProtocolFile;
+import com.zfoo.protocol.generate.GenerateProtocolPath;
+import com.zfoo.protocol.model.Pair;
+import com.zfoo.protocol.registration.IProtocolRegistration;
+import com.zfoo.protocol.registration.ProtocolRegistration;
+import com.zfoo.protocol.serializer.reflect.*;
+import com.zfoo.protocol.util.ClassUtils;
+import com.zfoo.protocol.util.FileUtils;
+import com.zfoo.protocol.util.IOUtils;
+import com.zfoo.protocol.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.zfoo.protocol.util.FileUtils.LS;
+import static com.zfoo.protocol.util.StringUtils.TAB;
+
+/**
+ * @author jaysunxiao
+ * @version 3.0
+ */
+public abstract class GenerateJsUtils {
+
+    private static String protocolOutputRootPath = "jsProtocol/";
+
+    private static Map<ISerializer, IJsSerializer> jsSerializerMap;
+
+    public static IJsSerializer jsSerializer(ISerializer serializer) {
+        return jsSerializerMap.get(serializer);
+    }
+
+    public static void init(GenerateOperation generateOperation) {
+        protocolOutputRootPath = FileUtils.joinPath(generateOperation.getProtocolPath(), protocolOutputRootPath);
+
+        FileUtils.deleteFile(new File(protocolOutputRootPath));
+        FileUtils.createDirectory(protocolOutputRootPath);
+
+        jsSerializerMap = new HashMap<>();
+        jsSerializerMap.put(BooleanSerializer.INSTANCE, new JsBooleanSerializer());
+        jsSerializerMap.put(ByteSerializer.INSTANCE, new JsByteSerializer());
+        jsSerializerMap.put(ShortSerializer.INSTANCE, new JsShortSerializer());
+        jsSerializerMap.put(IntSerializer.INSTANCE, new JsIntSerializer());
+        jsSerializerMap.put(LongSerializer.INSTANCE, new JsLongSerializer());
+        jsSerializerMap.put(FloatSerializer.INSTANCE, new JsFloatSerializer());
+        jsSerializerMap.put(DoubleSerializer.INSTANCE, new JsDoubleSerializer());
+        jsSerializerMap.put(CharSerializer.INSTANCE, new JsCharSerializer());
+        jsSerializerMap.put(StringSerializer.INSTANCE, new JsStringSerializer());
+        jsSerializerMap.put(ArraySerializer.INSTANCE, new JsArraySerializer());
+        jsSerializerMap.put(ListSerializer.INSTANCE, new JsListSerializer());
+        jsSerializerMap.put(SetSerializer.INSTANCE, new JsSetSerializer());
+        jsSerializerMap.put(MapSerializer.INSTANCE, new JsMapSerializer());
+        jsSerializerMap.put(ObjectProtocolSerializer.INSTANCE, new JsObjectProtocolSerializer());
+    }
+
+    public static void clear() {
+        jsSerializerMap = null;
+        protocolOutputRootPath = null;
+    }
+
+    public static void createProtocolManager(List<IProtocolRegistration> protocolList) throws IOException {
+        var list = List.of("javascript/buffer/ByteBuffer.js", "javascript/buffer/long.js", "javascript/buffer/longbits.js");
+        for (var fileName : list) {
+            var fileInputStream = ClassUtils.getFileFromClassPath(fileName);
+            var createFile = new File(StringUtils.format("{}/{}", protocolOutputRootPath, StringUtils.substringAfterFirst(fileName, "javascript/")));
+            FileUtils.writeInputStreamToFile(createFile, fileInputStream);
+        }
+
+        // 生成ProtocolManager.js文件
+        var protocolManagerTemplate = StringUtils.bytesToString(IOUtils.toByteArray(ClassUtils.getFileFromClassPath("javascript/ProtocolManagerTemplate.js")));
+
+        var importBuilder = new StringBuilder();
+        var initProtocolBuilder = new StringBuilder();
+        for (var protocol : protocolList) {
+            var protocolId = protocol.protocolId();
+            var protocolName = protocol.protocolConstructor().getDeclaringClass().getSimpleName();
+            var path = GenerateProtocolPath.getProtocolPath(protocol.protocolId());
+            if (StringUtils.isBlank(path)) {
+                importBuilder.append(StringUtils.format("import {} from './{}.js';", protocolName, protocolName)).append(LS);
+            } else {
+                importBuilder.append(StringUtils.format("import {} from './{}/{}.js';", protocolName, path, protocolName)).append(LS);
+            }
+
+            initProtocolBuilder.append(TAB).append(StringUtils.format("protocols.set({}, {});", protocolId, protocolName));
+        }
+
+        protocolManagerTemplate = StringUtils.format(protocolManagerTemplate, importBuilder.toString().trim(), initProtocolBuilder.toString().trim());
+        FileUtils.writeStringToFile(new File(StringUtils.format("{}/{}", protocolOutputRootPath, "ProtocolManager.js")), protocolManagerTemplate);
+    }
+
+    public static void createJsProtocolFile(ProtocolRegistration registration) throws IOException {
+        // 初始化index
+        GenerateProtocolFile.index.set(0);
+
+        var protocolId = registration.protocolId();
+        var registrationConstructor = registration.getConstructor();
+        var protocolClazzName = registrationConstructor.getDeclaringClass().getSimpleName();
+
+        var protocolTemplate = StringUtils.bytesToString(IOUtils.toByteArray(ClassUtils.getFileFromClassPath("javascript/ProtocolTemplate.js")));
+
+        var docTitle = docTitle(registration);
+        var valueOfMethod = valueOfMethod(registration);
+        var writeObject = writeObject(registration);
+        var readObject = readObject(registration);
+
+        protocolTemplate = StringUtils.format(protocolTemplate, docTitle, protocolClazzName
+                , valueOfMethod.getKey().trim(), valueOfMethod.getValue().trim(), protocolClazzName, protocolId, protocolClazzName
+                , writeObject.trim(), protocolClazzName, protocolClazzName, readObject.trim(), protocolClazzName);
+        var protocolOutputPath = StringUtils.format("{}/{}/{}.js", protocolOutputRootPath
+                , GenerateProtocolPath.getProtocolPath(protocolId), protocolClazzName);
+        FileUtils.writeStringToFile(new File(protocolOutputPath), protocolTemplate);
+    }
+
+    private static String docTitle(ProtocolRegistration registration) {
+        var protocolId = registration.getId();
+        var protocolDocument = GenerateProtocolDocument.getProtocolDocument(protocolId);
+        var docTitle = protocolDocument.getKey();
+        return docTitle;
+    }
+
+    private static Pair<String, String> valueOfMethod(ProtocolRegistration registration) {
+        var protocolId = registration.getId();
+        var fields = registration.getFields();
+
+        var fieldValueOf = StringUtils.joinWith(", ", Arrays.stream(fields).map(it -> it.getName()).collect(Collectors.toList()).toArray());
+        var fieldDefinitionBuilder = new StringBuilder();
+
+        var protocolDocument = GenerateProtocolDocument.getProtocolDocument(protocolId);
+        var docFieldMap = protocolDocument.getValue();
+        for (var field : fields) {
+            var propertyName = field.getName();
+            // 生成注释
+            var doc = docFieldMap.get(propertyName);
+            if (StringUtils.isNotBlank(doc)) {
+                Arrays.stream(doc.split(LS)).forEach(it -> fieldDefinitionBuilder.append(TAB).append(it).append(LS));
+            }
+
+            fieldDefinitionBuilder.append(TAB)
+                    .append(StringUtils.format("this.{} = {};", propertyName, propertyName))
+                    .append(" // ").append(field.getGenericType().getTypeName())// 生成类型的注释
+                    .append(LS);
+        }
+        return new Pair<>(fieldValueOf, fieldDefinitionBuilder.toString());
+    }
+
+    private static String writeObject(ProtocolRegistration registration) {
+        var fields = registration.getFields();
+        var fieldRegistrations = registration.getFieldRegistrations();
+        var jsBuilder = new StringBuilder();
+        for (var i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            var fieldRegistration = fieldRegistrations[i];
+            jsSerializer(fieldRegistration.serializer()).writeObject(jsBuilder, "packet." + field.getName(), 1, field, fieldRegistration);
+        }
+        return jsBuilder.toString();
+    }
+
+    private static String readObject(ProtocolRegistration registration) {
+        var fields = registration.getFields();
+        var fieldRegistrations = registration.getFieldRegistrations();
+        var jsBuilder = new StringBuilder();
+        for (var i = 0; i < fields.length; i++) {
+            var field = fields[i];
+            var fieldRegistration = fieldRegistrations[i];
+
+            var readObject = jsSerializer(fieldRegistration.serializer()).readObject(jsBuilder, 1, field, fieldRegistration);
+            jsBuilder.append(TAB).append(StringUtils.format("packet.{} = {};", field.getName(), readObject)).append(LS);
+        }
+        return jsBuilder.toString();
+    }
+}
