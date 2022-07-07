22acf58aa81cfaf85b8969756c7979d2440dba16
godotg <jaysunxiao.gmail.com>
2022-07-07 20:06:36 +0800

    perf[aes]: 提示Aes加密的性能

--- util/src/main/java/com/zfoo/util/security/AesUtils.java
+++ util/src/main/java/com/zfoo/util/security/AesUtils.java
@@ -20,6 +20,7 @@ import io.netty.util.concurrent.FastThreadLocal;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -46,13 +47,6 @@ public abstract class AesUtils {
      */
     private static final String ALGORITHM_STR = "AES/ECB/PKCS5Padding";
 
-    private static final FastThreadLocal<Cipher> LOCAL_CIPHER = new FastThreadLocal<Cipher>() {
-        @Override
-        protected Cipher initialValue() throws NoSuchPaddingException, NoSuchAlgorithmException {
-            return Cipher.getInstance(ALGORITHM_STR);
-        }
-    };
-
     static {
         try {
             KEY = new SecretKeySpec(KEY_STR.getBytes(StringUtils.DEFAULT_CHARSET_NAME), ALGORITHM);
@@ -60,6 +54,24 @@ public abstract class AesUtils {
             throw new RuntimeException(e);
         }
     }
+
+    private static final FastThreadLocal<Cipher> LOCAL_ENCRYPT_CIPHER = new FastThreadLocal<Cipher>() {
+        @Override
+        protected Cipher initialValue() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+            var cipher = Cipher.getInstance(ALGORITHM_STR);
+            cipher.init(Cipher.ENCRYPT_MODE, KEY);
+            return cipher;
+        }
+    };
+
+    private static final FastThreadLocal<Cipher> LOCAL_DECRYPT_CIPHER = new FastThreadLocal<Cipher>() {
+        @Override
+        protected Cipher initialValue() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+            var cipher = Cipher.getInstance(ALGORITHM_STR);
+            cipher.init(Cipher.DECRYPT_MODE, KEY);
+            return cipher;
+        }
+    };
 
 
     /**
@@ -81,8 +93,7 @@ public abstract class AesUtils {
 
     public static byte[] encrypt(byte[] bytes) {
         try {
-            var cipher = LOCAL_CIPHER.get();
-            cipher.init(Cipher.ENCRYPT_MODE, KEY);
+            var cipher = LOCAL_ENCRYPT_CIPHER.get();
             return cipher.doFinal(bytes);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -108,8 +119,7 @@ public abstract class AesUtils {
 
     public static byte[] decrypt(byte[] bytes) {
         try {
-            var cipher = LOCAL_CIPHER.get();
-            cipher.init(Cipher.DECRYPT_MODE, KEY);
+            var cipher = LOCAL_DECRYPT_CIPHER.get();
             return cipher.doFinal(bytes);
         } catch (Exception e) {
             throw new RuntimeException(e);
