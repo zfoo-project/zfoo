b3df782744c95179409d78a1f3d3434c0cfa345c
godotg <jaysunxiao.gmail.com>
2022-10-08 10:21:56 +0800

    perf[concurrent]: move concurrent collection to concurrent package

--- protocol/src/main/java/com/zfoo/protocol/collection/concurrent/ConcurrentArrayList.java
+++ protocol/src/main/java/com/zfoo/protocol/collection/concurrent/ConcurrentArrayList.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.zfoo.protocol.collection;
+package com.zfoo.protocol.collection.concurrent;
 
 import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
--- protocol/src/main/java/com/zfoo/protocol/collection/concurrent/ConcurrentHashSet.java
+++ protocol/src/main/java/com/zfoo/protocol/collection/concurrent/ConcurrentHashSet.java
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.zfoo.protocol.collection;
+package com.zfoo.protocol.collection.concurrent;
 
 import java.util.AbstractSet;
 import java.util.Iterator;
