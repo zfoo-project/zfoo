43000d733ef32c42d9ec290d88b62459b72861b9
godotg <jaysunxiao.gmail.com>
2022-12-02 18:08:12 +0800

    doc[deploy]: update document

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 
-# 服务器部署脚本，这个脚本封装了常用部署操作，不是一定要使用的
-
-# 注：
-# /deploy.sh脚本包括了，启动服务器，优雅停止服务器，更新程序文件，重新启动服务器，对应四个命令：start|stop|update|stopUpdateStart。
-# 其中stopUpdateStart会按照顺序执行，stop，update，start命令。
-
 # doc:
+# EN:Server deployment script, this script encapsulates common deployment operations, not necessarily used
+# CN:服务器部署脚本，这个脚本封装了常用部署操作，不是一定要使用的
 
+# EN:The deploy.sh script includes, start the server, gracefully stop the server, update the program file, restart the server, corresponding to four commands
+# CN:deploy.sh脚本包括了，启动服务器，优雅停止服务器，更新程序文件，重新启动服务器，对应四个命令：start|stop|update|stopUpdateStart。
+
+#######################################################################################################################
 # usage 1:
 # sh deploy.sh start /usr/local/tank/single-boot/single-boot-1.0.jar /usr/local/tank/single-boot
-# start启动服务器，绝对路径很重要，[/usr/local/tank/single-boot/single-boot-1.0.jar]是jar包的绝对路径，[/usr/local/tank/single-boot]是日志的输出的绝对路径
 
+# EN:start starts the server, the absolute path is very important,[/usr/local/tank/single-boot/single-boot-1.0.jar] is the absolute path of the jar package，
+# [/usr/local/tank/single-boot] is the absolute path to the output of the log
 
+# CN:start启动服务器，绝对路径很重要，[/usr/local/tank/single-boot/single-boot-1.0.jar]是jar包的绝对路径，[/usr/local/tank/single-boot]是日志的输出的绝对路径
+
+#######################################################################################################################
 # usage 2:
 # sh deploy.sh stop single
-# stop关闭服务器，会使用jps | grep single，抓取需要关闭的java进程，所以不需要输入single-boot的全名（不是绝对路径）
+
+# EN:stop closes the server, use command jps | grep single，grab the java process that needs to be closed, so you don't need to enter the full name of single-boot
+# If you have two launchers named [single-boot1.jar], [single-boot2], both will be turned off
+
+# CN:stop关闭服务器，会使用jps | grep single，抓取需要关闭的java进程，所以不需要输入single-boot的全名（不是绝对路径）
 # 如果你有两个名称为[single-boot1.jar]，[single-boot2]的启动程序，则会把这两个都关闭
 
-
+#######################################################################################################################
 # usage 3:
 # sh deploy.sh update /usr/local/tank/single-boot/single-boot-1.0.jar
+
+# EN:update updates the jar package. By default, the jar package under the root path [single-boot-1.0.jar] will be
+# used to update the jar package of [/usr/local/tank/single-boot/single-boot-1.0.jar]
+# In this way, the jar must be placed in the root directory (the topmost directory, not the root directory, don't make a mistake),
+# it is very important, because the jar of the root path is used by default to update the jar at the location you specify
+
 # update更新jar包，默认会使用根路径[/single-boot-1.0.jar]下的jar包去更新[/usr/local/tank/single-boot/single-boot-1.0.jar]的jar包
 # 此种方式jar必须放在 / 根目录(最上层的目录，不是root目录，不要弄错了)，非常重要，因为默认使用根路径的jar取更新你指定位置的jar
 
-
+#######################################################################################################################
 # usage 4:
 # sh deploy.sh stopUpdateStart /usr/local/tank/single-boot/single-boot-1.0.jar /usr/local/tank/single-boot
 # stopUpdateStart，按顺序执行命令stop，update，start，三合一的命令，因为有个update，所以必须把你的jar放在根目录（非常重要）
@@ -33,15 +47,12 @@
 # start同[usage 1]
 
 
-# 相关参数命令
-# java -XX:+PrintFlagsInitial，查看jvm全部参数的默认值
-#
 # @author godotg
 # @version 3.0
 
 
 if [ $# -lt 1 ]; then
-    echo "deploy.sh脚本使用错误，命令参数不合法"
+    echo "deploy.sh script use error, command parameter is illegal"
     echo "usage: sh deploy.sh start|stop|update|stopUpdateStart"
     exit 1
 fi
@@ -57,11 +68,11 @@ function waitAllProcessesExit() {
         runningProcesses=$(jps -lvm | grep ${1})
 
         if [ -n "${runningProcesses}" ]; then
-            echo "正在关闭以下Java进程${1}："
+            echo "The following Java processes are being shut down ${1}："
             echo "${runningProcesses}"
             sleep 3
         else
-            echo "已正常关闭所有${1}进程"
+            echo "Gracefully shut down ${1} processes"
             return
         fi
     done
@@ -72,27 +83,28 @@ function waitInfoLog() {
     while true; do
         infoLog=$(ls ./log | grep info)
         if [ -z "${infoLog}" ]; then
-            echo "正在等待info.log创建..."
+            echo "Waiting for info.log to be created..."
             sleep 1
         else
             return
         fi
 
         if [ -z "${infoLog}" ]; then
-            echo "正在等待info.log创建..."
+            echo "Waiting for info.log to be created..."
             sleep 2
         else
             return
         fi
 
         if [ -z "${infoLog}" ]; then
-            echo "正在等待info.log创建..."
+            echo "Waiting for info.log to be created..."
             sleep 3
         else
             return
         fi
 
         if [ -z "${infoLog}" ]; then
+            echo "If there is no response for a long time, solution 1: Please check the startup log, or whether there is a generated log file in the running directory"
             echo "如果长时间无响应，解决方法1：请检测启动log日志，或者运行目录是否有生成的日志文件"
             sleep 1
         else
@@ -100,6 +112,7 @@ function waitInfoLog() {
         fi
 
         if [ -z "${infoLog}" ]; then
+            echo "If there is no response for a long time, solution 2: Confirm whether the jvm parameter -Dspring.profiles.active=pro is correct and whether it is the operating environment configuration you expect"
             echo "如果长时间无响应，解决方法2：确认jvm参数-Dspring.profiles.active=pro是否正确，是否是你期望的运行环境配置"
             sleep 1
         else
@@ -107,6 +120,7 @@ function waitInfoLog() {
         fi
 
         if [ -z "${infoLog}" ]; then
+            echo "If there is no response for a long time, solution 3: directly use the command java -Dspring.profiles.active=pro -jar xxx.jar"
             echo "如果长时间无响应，解决方法3：直接使用命令 java -Dspring.profiles.active=pro -jar xxx.jar"
             sleep 1
         else
@@ -114,6 +128,7 @@ function waitInfoLog() {
         fi
 
         if [ -z "${infoLog}" ]; then
+            echo "If there is no response for a long time, solution 4: Run the main function in Idea, add the -Dspring.profiles.active=pro parameter, it can run in Idea and it must also run on the server"
             echo "如果长时间无响应，解决方法4：在Idea中运行main函数，加上-Dspring.profiles.active=pro参数，在Idea中能够运行也一定能在服务器运行"
             sleep 1
         else
@@ -129,44 +144,44 @@ function stop() {
     pids=$(jps | grep ${1} | awk '{print $1}' | paste -d " " -s)
 
     if [ -z "${pids}" ]; then
+        echo "Did not find any Java process containing the ${1} keyword"
         echo "没有找到任何包含${1}关键字的Java进程"
         return
     fi
 
     echo "**************************  cpu  **************************"
     uptime
-    echo "注："
-    echo "uptime查看cpu的负载情况，load avarage分别是1分钟，5分钟，15分钟内系统的load值"
-    echo "一般load值不大于3，我们就认为它的负载是正常的，大于了3就要想办法降低系统的负载"
+    echo "note:"
+    echo "uptime查看cpu的负载情况，load avarage分别是1分钟，5分钟，15分钟内系统的load值，一般load值不大于3，我们就认为它的负载是正常的，大于了3就要想办法降低系统的负载"
 
-    echo "**************************  内存  **************************"
+    echo "**************************  memory  **************************"
     free -m
-    echo "注："
+    echo "note:"
     echo "free查看内存的使用情况，重点关注swap内存使用，swap大表示物理内存不够用，这时候容易导致OOM异常"
 
-    echo "**************************  磁盘  **************************"
+    echo "**************************  disk  **************************"
     df -h
-    echo "注："
+    echo "note:"
     echo "df查看磁盘的使用情况，应该特别关注日志和数据库的挂载路径的使用情况"
 
-    echo "**************************  网络  **************************"
+    echo "**************************  network  **************************"
     sar -n DEV 1 2
-    echo "注："
+    echo "note:"
     echo "sar查看网络的使用情况，可以通过网络设备的吞吐量，判断网络设备是否已经饱和。"
 
-    echo "**************************系统日志**************************"
+    echo "**************************system log**************************"
     dmesg | tail -n 20
-    echo "注："
+    echo "note:"
     echo "df查看系统日志的最后20行，主要看有没有严重的系统问题"
 
-    echo "**************************jmap实例**************************"
+    echo "**************************jmap**************************"
     for pid in ${pids}; do
-        echo "${pid}->进程的class实例数量前20信息"
+        echo "${pid}->Information about the top 20 class instances of the process"
         jmap -histo ${pid} | head -n 20
         echo -e "\n"
     done
 
-    echo "********************开始执行终止Java进程命令********************"
+    echo "********************stop process********************"
     echo "kill -15 $pids"
     kill -15 ${pids}
 
@@ -179,7 +194,7 @@ function update() {
     local jarPath=${1}
 
     if [ -z "${jarPath}" ]; then
-        echo "需要被更新的jar路径不能为空"
+        echo "The jar path to be updated cannot be empty"
         echo "usage: sh deploy.sh update jarPath"
         exit 1
     fi
@@ -190,7 +205,7 @@ function update() {
     # echo ${jarFilePath}
     # echo ${jarName}
 
-    echo "更新之前源文件和目标文件信息:"
+    echo "Source and target file information before updating:"
     local fileInfo=$(ls -lh "/${jarName}")
     echo "${fileInfo}"
 
@@ -205,7 +220,7 @@ function update() {
     mkdir -p ${jarFilePath}
     cp "/${jarName}" ${jarFilePath}
 
-    echo "更新之后源文件和目标文件信息:"
+    echo "Source and target file information after update:"
     fileInfo=$(ls -lh "/${jarName}")
     echo "${fileInfo}"
     fileInfo=$(ls -lh ${jarPath})
@@ -220,13 +235,13 @@ function start() {
     local logPath=${2}
 
     if [ -z "${jarPath}" ]; then
-        echo "参数错误：启动的jar包名称不能为空"
+        echo "Parameter error: The name of the jar package to start cannot be empty"
         echo "usage: sh deploy.sh start jarPath logPath"
         exit 1
     fi
 
     if [ -z "${logPath}" ]; then
-        echo "参数错误：启动的日志路径不能为空"
+        echo "Parameter error: The log path for startup cannot be empty"
         echo "usage: sh deploy.sh start jarPath logPath"
         exit 1
     fi
@@ -236,7 +251,7 @@ function start() {
 
     local currentPath
     currentPath=$(pwd)
-    echo "pwd当前的运行路径：${currentPath}"
+    echo "pwd current path：${currentPath}"
 
     if [ ! -f "${jarPath}" ]; then
         echo "文件不存在：启动的jar包不存在，请检查jar的路径格式是否是绝对路径"
@@ -248,7 +263,7 @@ function start() {
     # 输出到文件  >> output.log 2>&1 &
     nohup java -Dspring.profiles.active=pro -XX:InitialHeapSize=1g -XX:MaxHeapSize=1g -XX:AutoBoxCacheMax=20000 -XX:+UseStringDeduplication -XX:+HeapDumpOnOutOfMemoryError -Djdk.attach.allowAttachSelf=true -Dfile.encoding=UTF-8 -jar ${jarPath} >/dev/null 2>&1 &
 
-    # 如果没有info的log，则一直等待
+    # If there is no info log, keep waiting
     waitInfoLog
 
     if [ -z "${3}" ]; then
@@ -258,21 +273,21 @@ function start() {
         local jarName=${jarPath##*/}
         local runningProcesses
         runningProcesses=$(jps -lvm | grep ${jarName})
-        echo "**************************jps信息**************************"
+        echo "**************************jps**************************"
         echo "${runningProcesses}"
 
         sleep 4
-        echo "************************jstat编译统计信息************************"
+        echo "************************jstat************************"
         local pids
         pids=$(jps | grep ${jarName} | awk '{print $1}' | paste -d " " -s)
         for pid in ${pids}; do
-            echo "${pid}->进程的编译统计信息"
+            echo "${pid}->Process compilation statistics"
             jstat -compiler ${pid}
             echo -e "\n"
         done
 
         sleep 6
-        echo "**************************启动日志**************************"
+        echo "**************************start log**************************"
         tail -n 40 log/info.log
 
         exit 0
@@ -285,33 +300,33 @@ function stopUpdateStart() {
     local logPath=${2}
 
     if [ -z "${jarPath}" ]; then
-        echo "启动的路径不能为空"
+        echo "The startup path cannot be empty"
         echo "usage: sh deploy.sh start jarPath logPath"
         exit 1
     fi
 
     if [ -z "${logPath}" ]; then
-        echo "启动的jar包名称不能为空"
+        echo "The jar package name to start cannot be empty"
         echo "usage: sh deploy.sh start jarPath logPath"
         exit 1
     fi
 
-    echo "开始按顺序执行任务： stop -> update -> start"
+    echo "Start executing tasks： stop -> update -> start"
     echo -e "\n\n"
     local jarFilePath=${jarPath%/*}
     local jarName=${jarPath##*/}
 
-    # 先停止
+    # stop first
     stop ${jarName}
     echo
     echo
     echo
-    # 再更新
+    # update second
     update ${jarPath}
     echo
     echo
     echo
-    # 最后启动
+    # last start
     start ${jarPath} ${logPath} ${3}
 }
 
@@ -337,9 +352,9 @@ function optimizeLinux() {
     echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
     echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
     echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-    echo "性能调优预留方法"
 }
-# 立即执行调优
+
+# run optimizeLinux method immediately
 optimizeLinux
 
 case ${command} in
@@ -356,7 +371,7 @@ case ${command} in
     stopUpdateStart ${2} ${3} ${4}
     ;;
 *)
-    echo "命令无法识别: ${1}"
+    echo "command not recognized: ${1}"
     echo "usage: sh deploy.sh start|stop|udpate|stopUpdateStart"
     ;;
 esac
