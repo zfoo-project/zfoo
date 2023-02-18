608d5f0ad01694fe81fe418f9bb6d30a166c9552
aoyu <1790306716@qq.com>
2023-02-18 22:37:38 +0800

    test[protocol]test serialization and deserialization performance of fields(int,Integer,float)

---
+++ protocol/src/test/java/com/zfoo/protocol/field/packet/IntegerObject.java
@@ -0,0 +1,120 @@
+package com.zfoo.protocol.field.packet;
+
+import com.zfoo.protocol.IPacket;
+
+import java.util.Objects;
+
+public class IntegerObject implements IPacket {
+    private Integer a;
+
+    private Integer b;
+
+    private Integer c;
+
+    private Integer d;
+
+    private Integer e;
+
+    private Integer f;
+
+    private Integer g;
+
+    private Integer h;
+
+    private Integer i;
+
+    private Integer j;
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+
+    public Integer getC() {
+        return c;
+    }
+
+    public void setC(Integer c) {
+        this.c = c;
+    }
+
+    public Integer getD() {
+        return d;
+    }
+
+    public void setD(Integer d) {
+        this.d = d;
+    }
+
+    public Integer getE() {
+        return e;
+    }
+
+    public void setE(Integer e) {
+        this.e = e;
+    }
+
+    public Integer getF() {
+        return f;
+    }
+
+    public void setF(Integer f) {
+        this.f = f;
+    }
+
+    public Integer getG() {
+        return g;
+    }
+
+    public void setG(Integer g) {
+        this.g = g;
+    }
+
+    public Integer getH() {
+        return h;
+    }
+
+    public void setH(Integer h) {
+        this.h = h;
+    }
+
+    public Integer getI() {
+        return i;
+    }
+
+    public void setI(Integer i) {
+        this.i = i;
+    }
+
+    public Integer getJ() {
+        return j;
+    }
+
+    public void setJ(Integer j) {
+        this.j = j;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IntegerObject that = (IntegerObject) o;
+        return a.equals(that.a) && b.equals(that.b) && c.equals(that.c) && d.equals(that.d) && e.equals(that.e) && f.equals(that.f) && g.equals(that.g) && h.equals(that.h) && i.equals(that.i) && j.equals(that.j);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c, d, e, f, g, h, i, j);
+    }
+}
