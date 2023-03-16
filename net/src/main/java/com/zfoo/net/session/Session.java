ee09c3a84eb4f242c24b62dcdb7809b19d7553e7
godotg <jaysunxiao@gmail.com>
2023-03-16 11:55:15 +0800

    chore[session]: code comments

--- net/src/main/java/com/zfoo/net/session/Session.java
+++ net/src/main/java/com/zfoo/net/session/Session.java
@@ -34,6 +34,8 @@ public class Session implements Closeable {
 
     private Channel channel;
 
+    // ------------------------------------------------------------------------------------------------------------
+    // The following are extra parameters, add them yourself if necessary（下面都是额外参数，有需要的自己添加）
     /**
      * EN:The default user ID is an ID greater than 0, or equal 0 if there is no login, user extra parameters
      * CN:默认用户的id都是大于0的id，如果没有登录则等于0，用户额外参数
