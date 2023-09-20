62c36b1749804cf7deac82cc5455d4e8f49c078a
sun <jaysunxiao@gmail.com>
2023-09-20 17:46:55 +0800

    feat[thread local]: FastThreadLocalAdapter

---
+++ protocol/src/main/java/com/zfoo/protocol/util/FastThreadLocalAdapter.java
@@ -0,0 +1,29 @@
+package com.zfoo.protocol.util;
+
+import io.netty.util.concurrent.FastThreadLocal;
+
+import java.util.function.Supplier;
+
+/**
+ * @author godotg
+ */
+public class FastThreadLocalAdapter<T> {
+
+    private FastThreadLocal<T> fastThreadLocal;
+
+    private Supplier<T> supplier;
+
+    public FastThreadLocalAdapter(Supplier<T> supplier) {
+        this.supplier = supplier;
+        this.fastThreadLocal = new FastThreadLocal<>() {
+            @Override
+            protected T initialValue() {
+                return supplier.get();
+            }
+        };
+    }
+
+    public T get() {
+        return Thread.currentThread().isVirtual() ? supplier.get() : fastThreadLocal.get();
+    }
+}
