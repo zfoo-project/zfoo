b042863fd1f6bf66715eb43da4d23f23e6c93741
godotg <jaysunxiao@gmail.com>
2023-12-11 12:54:31 +0800

    test[protobuf]: support record class generation from proto

--- protocol/src/test/java/com/zfoo/protocol/generate/GenerateProtobufTesting.java
+++ protocol/src/test/java/com/zfoo/protocol/generate/GenerateProtobufTesting.java
@@ -22,11 +22,12 @@ import org.junit.Test;
 public class GenerateProtobufTesting {
 
     @Test
-    public void testAllTypeTest() {
+    public void generateClassFromProto() {
         var buildOption = new PbGenerateOperation();
         buildOption.setProtoPath("D:\\github\\zfoo\\protocol\\src\\test\\protobuf");
         buildOption.setOutputPath("D:\\github\\zfoo\\protocol\\src\\test\\zfoopb");
         buildOption.setJavaPackage("com.zfoo.protocol.generate.test");
+//        buildOption.setRecordClass(true);
 
         GeneratePbUtils.create(buildOption);
     }
