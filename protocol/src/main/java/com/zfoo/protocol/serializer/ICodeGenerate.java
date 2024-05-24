05f988f1b6432daf7704ec852af177948630e88a
godotg <jaysunxiao@gmail.com>
2024-05-24 11:02:29 +0800

    chore[doc]: generate doc

--- protocol/src/main/java/com/zfoo/protocol/serializer/ICodeGenerate.java
+++ protocol/src/main/java/com/zfoo/protocol/serializer/ICodeGenerate.java
@@ -7,6 +7,15 @@ import java.io.IOException;
 import java.util.List;
 
 /**
+ * 协议修改过后的测试流程：
+ * 1.测试Java的老协议和新协议的兼容性
+ * 2.测试Java在字节码增强，CutDown，反射三种情况下的代码下的协议解析的准确性
+ * 3.测试resource/compatible中有5种协议兼容情况需要测试
+ * 4.1和2总共有2*3*5=30种情况
+ * <p>
+ * 协议生成修改后的测试流程：
+ * 同上，除了反射，总共有2*2*5=20种情况
+ *
  * @author godotg
  */
 public interface ICodeGenerate {
