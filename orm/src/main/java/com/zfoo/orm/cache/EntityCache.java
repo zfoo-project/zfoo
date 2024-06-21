22d6ad4ee0728caf495c45c61ee6ea7aeaeb1998
godotg <jaysunxiao@gmail.com>
2024-06-21 18:35:34 +0800

    perf[orm]: update entity cache time when call updateNow method

--- orm/src/main/java/com/zfoo/orm/cache/EntityCache.java
+++ orm/src/main/java/com/zfoo/orm/cache/EntityCache.java
@@ -205,12 +205,18 @@ public class EntityCache<PK extends Comparable<PK>, E extends IEntity<PK>> imple
     public void updateNow(E entity) {
         var cachePnode = fetchCachePnode(entity, true);
         OrmContext.getAccessor().update(cachePnode.getEntity());
+        var currentTime = TimeUtils.now();
+        cachePnode.setWriteToDbTime(currentTime);
+        cachePnode.setModifiedTime(currentTime);
     }
 
     @Override
     public void updateNowUnsafe(E entity) {
         var cachePnode = fetchCachePnode(entity, false);
         OrmContext.getAccessor().update(cachePnode.getEntity());
+        var currentTime = TimeUtils.now();
+        cachePnode.setWriteToDbTime(currentTime);
+        cachePnode.setModifiedTime(currentTime);
     }
 
     @Override
