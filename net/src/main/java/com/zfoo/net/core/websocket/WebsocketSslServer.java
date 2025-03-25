032aa6c17083af802a6da77a16966ae9a7c9308e
jaysunxiao <jaysunxiao@gmail.com>
2025-03-25 10:38:58 +0800

    doc: netty ssl config

--- net/src/main/java/com/zfoo/net/core/websocket/WebsocketSslServer.java
+++ net/src/main/java/com/zfoo/net/core/websocket/WebsocketSslServer.java
@@ -39,16 +39,17 @@ public class WebsocketSslServer extends AbstractServer<SocketChannel> {
     private SslContext sslContext;
 
     /**
-     * 阿里云证书转换：
-     * 进入阿里云证书下载页面，要下载两份，一个是nginx的，一个是tomcat的。
-     * 1.下载的nginx格式的证书，里面包含pem和key两种格式的文件。其中pem格式我们可以直接用，key格式的Java无法直接使用。
+     * 方式一：
+     * 下载的nginx格式的证书，里面包含pem和key两种格式的文件。其中pem格式我们可以直接用，key格式的Java无法直接使用。
+     * 我们使用OpenSSL将PKCS1 key格式证书换一个可以使用的PKCS8 key文件出来:
+     * openssl pkcs8 -topk8 -nocrypt -in ws.jiucai.fun.key -out ws.jiucai.fun.key.pem
      * <p>
-     * 2.下载的tomcat格式的证书，我们使用OpenSSL将前面下载tomcat格式证书pfx文件转换一个可以使用的key文件出来:
-     * openssl pkcs12 -in xxx.pfx -nocerts -nodes -out new_xxx.key
+     * 方式二：阿里云证书转换
+     * 进入阿里云证书下载页面，如下图。要下载两份，一个是tomcat的，一个是nginx的。
+     * 下载的nginx格式的证书，里面包含pem和key两种格式的文件。其中pem格式我们可以直接用，key格式的Java无法直接使用。
+     * 我们使用OpenSSL将前面下载tomcat格式证书pfx文件转换一个可以使用的key文件出来:
+     * openssl pkcs12 -in ws.jiucai.fun.pfx -nocerts -nodes -out ws.jiucai.fun.key
      * openssl会让输入密码，密码是tomcat下载文件里的pfx-password.txt
-     * Linux系统一般自带openssl工具，把证书上传到一台Linux服务器去转换一下就可以了。
-     * <p>
-     * 3.第一步获得的pem证书和第二步获得的key证书就是下面这个构造方法要使用的
      */
     public WebsocketSslServer(HostAndPort host, InputStream pem, InputStream key) {
         super(host);
